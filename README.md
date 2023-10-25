44a341feb5a6d1c407c57548ebb5a280689a76ca
Josh T <joshthyng187@gmail.com>
2023-10-25 18:35:52 -0400

    myTreeIterator - Josh

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab_5W
+
+Josh - Subtask 2: Done (Will test after other parts are done)
---
+++ myTreeIterator.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import java.util.ArrayList;
+
+interface ITreeIterator {
+    int[] SequenceArray(BinarySearchTree tree);
+}
+
+//myTreeIterator class implements the ITreeIterator interface.
+class myTreeIterator implements ITreeIterator {
+    
+    //method that implements the SequenceArray method
+    @Override
+    public int[] SequenceArray(BinarySearchTree tree) {
+        //this will hold temporarily hold the values of the tree
+        List<Integer> infoList = new ArrayList<>();
+        
+        orderedTraversal(tree.root, infoList);
+        
+        //converts the list of ints into array to return
+        return infoList.stream().mapToInt(i -> i).toArray();
+    }
+
+    //recursively performs in-order traversal on the tree.
+    private void orderedTraversal(TreeNode node, List<Integer> list) {
+        //base case
+        if (node == null) {
+            return;
+        }
+        //recursively goes to left subtree
+        orderedTraversal(node.left, list);
+        //add current node
+        list.add(node.value);
+        //recursively goes to right subtree
+        orderedTraversal(node.right, list);
+    }
+}
